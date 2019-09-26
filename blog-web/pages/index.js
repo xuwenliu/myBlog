7b13c4dd4bc08f902807dba7637832cc71de13d2
js114 <1916579055@qq.com>
2019-09-26 17:36:08 +0700

    feat(add) 初始化blog-web项目

---
+++ blog-web/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Head from 'next/head';
+import { Button } from 'antd';
+const Home = () => (
+  <>
+    <Head>
+      <title>xuwenliu的博客</title>
+      <Button>2345</Button>
+    </Head>
+  </>
+)
+export default Home
